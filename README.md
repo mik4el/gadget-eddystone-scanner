e741eae9c7c2e77dc80ff6e8e706b4faa3427706
Mikael Andersson <mikael.andersson@volumental.com>
2017-01-08 22:20:36 +0100

    posting data to m4bd.se

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # gadget-eddystone-scanner
 Scan nearby eddystone beacons with TLM frames and upload to web service
 
-## Setup
+## To run
 Requires python <3.0. See further requirements on https://github.com/sandeepmistry/noble
 
 1. `npm install`
+1. Export credentials as environment variables
+1. `node node scan_and_post_eddystone_beacons.js`
 
 ## Debugging
 * Scan for eddystone beacons: `node scan_eddystone_beacons.js`
 * Broadcast a test beacon with URL and TLM: `node broadcast_eddystone_beacon.js`
+
+## Todos
+* Make docker setup and deploy and test on raspi
---
+++ scan_and_post_eddystone_beacons.js
@@ -0,0 +1,77 @@
+var EddystoneBeaconScanner = require('eddystone-beacon-scanner')
+var request = require('request');
+const assert = require('assert');
+
+EddystoneBeaconScanner.on('found', function(beacon) {
+  console.log('found Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
+  postBeaconToWebService(beacon);
+});
+
+EddystoneBeaconScanner.on('updated', function(beacon) {
+  console.log('updated Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
+  postBeaconToWebService(beacon);
+});
+
+EddystoneBeaconScanner.on('lost', function(beacon) {
+  console.log('lost Eddystone beacon:\n', JSON.stringify(beacon, null, 2));
+});
+
+var token = null;
+
+function postBeaconToWebService(beacon) {
+	if (token === null) {
+		console.log("No token...");
+		return
+	} 
+	
+	gadget_slug = "eddystone-scanner"
+    post_url = process.env.GADGET_DATA_POSTER_URL + '/backend/api/v1/gadgets/' + gadget_slug + '/data/'
+    payload = {
+        'data': beacon,
+        'timestamp': '2017-01-08T00:00:00'
+    }
+    var options = {
+		url: post_url,
+		method: "POST",
+    	json: payload,
+    	headers: { 'Authorization': 'Bearer ' + token }
+	};
+	request(options, postBeaconToWebServiceCallback);
+}
+
+function postBeaconToWebServiceCallback(error, response, body) {
+	console.log(error, response, body);
+	if (!error && response.statusCode == 201) {
+		console.log("Beacon posted...");
+	} else {
+		console.log("Could not post beacon...", body);	
+		process.exit(1);
+	}	
+}
+
+function getWebServiceToken() {
+	console.log("Getting token...")
+	var options = {
+		url: process.env.GADGET_DATA_POSTER_URL + '/backend/api-token-auth/',
+		method: "POST",
+    	json: { 'username': process.env.GADGET_DATA_POSTER_USERNAME, 'password': process.env.GADGET_DATA_POSTER_PASSWORD }
+	};
+	request(options, getWebServiceTokenCallback);
+}
+
+function getWebServiceTokenCallback(error, response, body) {
+	if (!error && response.statusCode == 200) {
+		token = body.token;
+		EddystoneBeaconScanner.startScanning(true);
+		console.log("Started scanning...");	
+	} else {
+		console.log("Could not get token...", body);
+		process.exit(1);
+	}
+}
+
+assert(process.env.GADGET_DATA_POSTER_URL);
+assert(process.env.GADGET_DATA_POSTER_USERNAME);
+assert(process.env.GADGET_DATA_POSTER_PASSWORD);
+
+getWebServiceToken();
